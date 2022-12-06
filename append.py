42a145441c406923d1718b3456d6170456d74927
TatianaCruz2 <tscruz3@misena.edu.co>
2022-12-06 09:43:12 -0500

    metodo append

---
+++ append.py
@@ -0,0 +1,7 @@
+palabras = ["gato", "perro"]
+nuevoArreglo = []
+for i in palabras:
+    for j in i:
+        nuevoArreglo.append(j)
+
+print("El nuevo arreglo es:", nuevoArreglo)
---
+++ recorrer.py
@@ -0,0 +1,7 @@
+for i in [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]:
+    print(i)
+for i in range(5):
+    resultadoMultipli = i * 2
+    print("El primer resultado es: ", resultadoMultipli)
+    resultadoPotencia = resultadoMultipli**2
+    print("El resultado es: ", resultadoPotencia)
